d722765ff4148c39680fe3001854df4a223c7082
dxtdbdd <2192383208@qq.com>
2020-04-13 13:52:39 +0800

    just a test

---
+++ test6.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/*
+int getMaxValue(v1, v2);
+
+int main()
+
+{
+  int   maxValue = getMaxValue(100, 200);
+  printf("The max value is %d", maxValue);
+  return 0;
+}
+int getMaxValue(int v1, int v2)
+{
+int v3 = 0;
+if(v1 > v2)
+{
+v3 = v1;
+}
+else
+{
+v3=v2;
+}
+    return v3;
+}
+*/
